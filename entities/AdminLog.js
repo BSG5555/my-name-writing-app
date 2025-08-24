b1cd9419c51beaa2f6702dcdd236cf6c770bca31
BSG5555 <brainsttudioglobal@gmail.com>
2025-08-24 21:00:25 +0530

    Refactor AdminLog to use class syntax

--- entities/AdminLog.js
+++ entities/AdminLog.js
@@ -1,5 +1,8 @@
-export const AdminLog = {
-  async create(entry) {
+export class AdminLog {
+  static async create(entry) {
+    // Placeholder logging
+    // eslint-disable-next-line no-console
+    console.log('AdminLog entry created:', entry);
     return { id: Date.now().toString(), ...entry };
   }
-};
+}
